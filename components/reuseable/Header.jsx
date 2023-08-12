c4ac7ba01cf5057a6413b7a22f50b8cc1ac234f7
Furqan558 <furqanhameed558@gmail.com>
2023-08-12 13:29:28 +0500

    fixed revisions v3

--- components/reuseable/Header.jsx
+++ components/reuseable/Header.jsx
@@ -29,33 +29,43 @@ const Header = () => {
 							<NavDropdown.Item href="/products/Power%20Systems%20for%20Telecom">
 								Power Systems for Telecom
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/products/Batteries%20&%20backup%20Power%20Solutions">
 								Battries & Backup Power Solutions
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/products/Data%20Centre%20Rack%20and%20Cabinets">
 								Data Centre Rack and Cabinets
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/products/UPS%20&%20Inverter%20System%20Solutions">
 								UPS & Inverter System Solutions
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/products/Fiber%20Patch%20Cords">
 								Fiber Patch Cords
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/products/OSP%20and%20ISP%20Passive%20materials">
 								OSP and ISP Passive Materials
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/products/Indoor%20Building%20Solutions%20Components">
 								Indoor Building Solutions Components
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/products/RF%20Antenna%20&%20Components">
 								RF Antenna & Components
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/products/AC%20or%20DC%20Power%20Cable">
 								AC or DC Power Cable
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/products/Solar%20power%20Solutions">
 								Solar Power Solutions
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/products/EV%20charging%20Stations">
 								EV Charging Stations
 							</NavDropdown.Item>
@@ -68,18 +78,23 @@ const Header = () => {
 							<NavDropdown.Item href="/services/Turnkey%20Solutions">
 								Turnkey Solutions
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/services/Civil%20Structure%20Installation%20for%20all%20kinds%20of%20Towers%20&%20Poles">
 								Civil Structure Installation
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/services/Telecom%20Installation">
 								Telecom Installation
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/services/In-building%20Solutions">
 								In-building Solutions
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/services/Core%20Network%20Installation">
 								Core Network Installation
 							</NavDropdown.Item>
+							<NavDropdown.Divider />
 							<NavDropdown.Item href="/services/Shelters%20&%20Diesel%20Generators">
 								Shelters & Diesel Generators
 							</NavDropdown.Item>
