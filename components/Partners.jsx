ff59a0666299ddd41ef4dd61d392f905fe69a99d
Furqan558 <furqanhameed558@gmail.com>
2023-09-10 00:16:17 +0500

    added partners logos

--- components/Partners.jsx
+++ components/Partners.jsx
@@ -35,20 +35,80 @@ const Partners = () => {
         <h2>Our Partners --</h2>
         <div className={styles["logo-slider"]}>
           <div>
-            <img src="clientLogo.png" alt="Logo 1" />
-            <img src="clientLogo.png" alt="Logo 2" />
-            <img src="clientLogo.png" alt="Logo 3" />
-            <img src="clientLogo.png" alt="Logo 4" />
-            <img src="clientLogo.png" alt="Logo 5" />
-            <img src="clientLogo.png" alt="Logo 6" />
+            <img
+              src="/partners/Dell_Logo.png"
+              alt="Logo 1"
+              width={"75px"}
+              height={"75px"}
+            />
+            <img
+              src="/partners/footer_logo.png"
+              alt="Logo 2"
+              width={"75px"}
+              height={"75px"}
+            />
+            <img
+              src="/partners/Hansen-Reverse-Colour.svg"
+              alt="Logo 3"
+              width={"75px"}
+              height={"75px"}
+            />
+            <img
+              src="/partners/icomLogo.png"
+              alt="Logo 4"
+              width={"75px"}
+              height={"75px"}
+            />
+            <img
+              src="/partners/logo.png"
+              alt="Logo 5"
+              width={"75px"}
+              height={"75px"}
+            />
+            <img
+              src="/partners/symbolmark.png"
+              alt="Logo 6"
+              width={"75px"}
+              height={"75px"}
+            />
           </div>
           <div>
-            <img src="clientLogo.png" alt="Logo 1" />
-            <img src="clientLogo.png" alt="Logo 2" />
-            <img src="clientLogo.png" alt="Logo 3" />
-            <img src="clientLogo.png" alt="Logo 4" />
-            <img src="clientLogo.png" alt="Logo 5" />
-            <img src="clientLogo.png" alt="Logo 6" />
+            <img
+              src="/partners/LOGO_header.57e0e23.png"
+              alt="Logo 1"
+              width={"75px"}
+              height={"75px"}
+            />
+            <img
+              src="/partners/img.jpg"
+              alt="Logo 2"
+              width={"75px"}
+              height={"75px"}
+            />
+            <img
+              src="/partners/VMware-logo.png"
+              alt="Logo 3"
+              width={"75px"}
+              height={"75px"}
+            />
+            <img
+              src="/partners/ISC-logo-1.png"
+              alt="Logo 4"
+              width={"75px"}
+              height={"75px"}
+            />
+            <img
+              src="/partners/HON logo_200x37 2.png"
+              alt="Logo 5"
+              width={"75px"}
+              height={"75px"}
+            />
+            <img
+              src="/partners/Hengxin Technology Limited.png"
+              alt="Logo 6"
+              width={"75px"}
+              height={"75px"}
+            />
           </div>
         </div>
       </Container>
