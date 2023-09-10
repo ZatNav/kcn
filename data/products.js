f12364a70f6cee698292f719f0f29808775a2ee4
Furqan558 <furqanhameed558@gmail.com>
2023-09-10 11:33:49 +0500

    added products and services content v2

--- data/products.js
+++ data/products.js
@@ -61,6 +61,13 @@ export const products = [
     name: "AC or DC Power Cable",
     description:
       "We provide high-quality AC/DC Power Cables to efficiently transmit electrical power to your telecom equipment. Our cables are designed for durability and safe power distribution.",
+    technical: `Single Core PVC Insulated Cables: Range (Nominal Conductor Area mm square varies from 1.5 to 1000), Maximum Diameter mm (3.2 to 51.0), Approximate Weight kg/km (21 to 9800)
+    "UPS and Inverter Systems" 
+    Invertor system overview (Maintenance Bypass Switch, Static Transfer Switch, MC1000 Controller, Network Management Card, Inverter Shelves, INV4815E 1.5kVA Inverter Modules)
+    ⦁	Eaton 5P UPS
+    ⦁	Eaton 93PM UPS
+    ⦁	Eaton 9155 Marine UPS
+    `,
   },
   {
     id: 10,
@@ -88,30 +95,48 @@ export const products = [
     id: 12,
     imageSrc: "product1.png",
     name: "Batteries and backup solutions",
-    description: `Our Battery Energy Storage System (BESS) offers cutting-edge battery technology and backup solutions to ensure uninterrupted power supply for your critical infrastructure. Whether it's a telecom network, data center, or any other application, our BESS provides reliable energy storage to keep your operations running smoothly.
-    "Lithium-Ion Batteries"
-⦁	Smart Power 48V100Ah(3U) A powerful 48V LiFePO4 battery product, has been especially designed to provide power backup for remote or outdoor telecom sites, off-grid, on-grid and household usage energy storage systems. Its modular design enables parallel installation meeting also the needs for larger power back up.­
-Benefits: 1- Increased energy in given space 2- Easy installation and upscaling 3- High operational reliability 4- Optimized supervision strategy through remote control/diagnostic 5- Excellent long lifetime 6- Built-in intelligent BMS to protect the battery pack at any time and prolong its service life.
-⦁	Smart Power 48V150Ah
-⦁	OPzV Series
-"Distributed LI-ION Battery System for 5G" 48V 20/30/40/50AH
-"Lithium Ion Batteries" 
-⦁	SDA 10-48100
-⦁	SDA 10-48200 
-Applications: 1- Backup power supply for FTTB, FTTH, ONU, EPON 2- Applicable to stable grid, half-grid and other scenes
-Advantages: 
-⦁	High energy desity: more energy with less weight and footprint
-⦁	High charge and discharge currents (short cicuit period)
-⦁	Long battery life (upto 3 times the battery life of a conventional battery)
-⦁	High efficiency between charging and discharging 
-⦁	Higher continual power available
-⦁	Wide operating temperature
-⦁	Predictable end of life due BMS controller
-⦁	Multiple anti-theft solutions (optional): software, gyroscope, material, etc.
-⦁	Other functions (optional): Heating/LCD/Dry contact
-"Super Capacitor Batteries"
-⦁	Sirius Capacitor Module "Sirius" is super capicitor based storage that uses super capacitors as storage cells instead of chemical cells. Kilowatt Labs proprietary balancing, control and charge retention algorithms control the operation of the super capacitor based modules, making SIrius a safe, efficient and effective alternative to chemical batteries wherever
-`,
-    technical: ``,
+    description:
+      "Our Battery Energy Storage System (BESS) offers cutting-edge battery technology and backup solutions to ensure uninterrupted power supply for your critical infrastructure. Whether it's a telecom network, data center, or any other application, our BESS provides reliable energy storage to keep your operations running smoothly.",
+    technical: `Technical Specifications: DC Energy 7100WH, DC Voltage (Nominal) 48V, Equivalent Series Resistance (ESR) <mΩ, Technology: Supercapacitors, Type: Pouch cells
+    Charge Specifications: Charge current @ 1C : 148A, Maximum CHarge Current: 252A (1.7C), Maximum Charge Voltage Range: 40V - 57V 
+    Discharge Specifications: Discharge current @ 1C : 148A (1C), Maximum Discharge Current 252A (1.7C), Maximum Discharging Voltage range: 57V - 40V
+     
+    ⦁	SUPERNOVA 3400Wh48V: Super Capacitor energy storage module for Telecom & Off-Grid system
+    Technical Specifications: Voltahge (Nominal) 48V, Maximum charge voltage 54.6V, Discharge Cut-off Voltage 40V (Configurable), Total Energy 3400 Wh, Continuous Discharge current 72A, Peak Discharge current 122A (5sec), Continuous Charge rate 72A, Cut off current 2A, ESR/AC @ 1KHz <10mΩ
+    Physical Specifications: Weight 32kg (module) 33kg (with pack), Module casing material (MS/Aluminum), Terminal Type (Anderson), Dimensions (L x W x H) (440 x 466 x 176mm) Dimensions (L x W x H) pack (560 x 480 x 210)
+    ⦁	Super Battery: Charged in 60 seconds, bridging the gap between supercapacitors and batteries - Based on superca[acitor technology
+    Technical Specifications: High power density (10kW/kg), Increased energy density (65 Wh/kg), Long cycle life (50,000), Fast chargong rate (20 C continuous, 100 C peak), High inherent safety, High recyclability and sustainability, No Graphite, no Co, <5% Li
+    ⦁	SkelMod 54V299F
+    Technical Specifications: 54V DC niminal voltage, Ultra-low ESR, Long Lifetime - 1 milion duty cycles, Integrated Supercapacitor Management System for effective cell balancing, CAN bus communication, Natural cooling, High Power output, IP65
+    ⦁	SkelMod 51V166F
+    Technical Specifications: 51V DC nominal voltage, 166 F capacitance, IP65 protection, High power output
+    "Optical Passive Network Products" All images on PDF file.
+    "IBS DAS Products" 
+    LTE High Performance Passive Components: Power Splitter, Coupler, Hybrid Coupler, Load, Attenuator, Combiner
+    Feeder Cables and Accessories: All series coaxial feeder cable (7/8" feeder cable, 1/2" feeder cable, 1-5/8" feeder cable), All Series Connector (DIN-DIN, DIN-N, N-N)
+    Ultra Wide Band Indoor Antennas: Indoor omni antenna, Indoor panel antenna, Indoor MIMO omni antenna, Indoor MIMO panel antenna
+    `,
+    details: `"Lithium-Ion Batteries"
+    ⦁	Smart Power 48V100Ah(3U) A powerful 48V LiFePO4 battery product, has been especially designed to provide power backup for remote or outdoor telecom sites, off-grid, on-grid and household usage energy storage systems. Its modular design enables parallel installation meeting also the needs for larger power back up.­
+    Benefits: 1- Increased energy in given space 2- Easy installation and upscaling 3- High operational reliability 4- Optimized supervision strategy through remote control/diagnostic 5- Excellent long lifetime 6- Built-in intelligent BMS to protect the battery pack at any time and prolong its service life.
+    ⦁	Smart Power 48V150Ah
+    ⦁	OPzV Series
+    "Distributed LI-ION Battery System for 5G" 48V 20/30/40/50AH
+    "Lithium Ion Batteries" 
+    ⦁	SDA 10-48100
+    ⦁	SDA 10-48200 
+    Applications: 1- Backup power supply for FTTB, FTTH, ONU, EPON 2- Applicable to stable grid, half-grid and other scenes
+    Advantages: 
+    ⦁	High energy desity: more energy with less weight and footprint
+    ⦁	High charge and discharge currents (short cicuit period)
+    ⦁	Long battery life (upto 3 times the battery life of a conventional battery)
+    ⦁	High efficiency between charging and discharging 
+    ⦁	Higher continual power available
+    ⦁	Wide operating temperature
+    ⦁	Predictable end of life due BMS controller
+    ⦁	Multiple anti-theft solutions (optional): software, gyroscope, material, etc.
+    ⦁	Other functions (optional): Heating/LCD/Dry contact
+    "Super Capacitor Batteries"
+    ⦁	Sirius Capacitor Module "Sirius" is super capicitor based storage that uses super capacitors as storage cells instead of chemical cells. Kilowatt Labs proprietary balancing, control and charge retention algorithms control the operation of the super capacitor based modules, making SIrius a safe, efficient and effective alternative to chemical batteries wherever`,
   },
 ];
